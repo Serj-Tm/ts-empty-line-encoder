1cbaf3d97d4461a0f0886990217e2fffc176ec36
- <->
2019-07-02 22:11:00 +0300

    rename to gridpier

---
+++ gridpier-auth/auth.ts
@@ -0,0 +1,56 @@
+﻿
+function getCookies() {
+  let cookies:any = {};
+  document.cookie.split(';').forEach(function (el) {
+    let [k, v] = el.split('=');
+    cookies[k.trim()] = v;
+  })
+  return cookies;
+}
+
+export function getAuth(): Auth | null {
+  //сброс авторизации при отсутствии авторизации в asp.net core
+  if (getCookies()['.AspNetCore.Cookies'] == null)
+    clearAuth();
+
+
+  let storage = sessionStorage;
+  if (localStorage != null && localStorage['mirror.auth'] != null)
+    storage = localStorage;
+
+  const auth = storage['mirror.auth'];
+  if (auth == null)
+    return null;
+
+  return JSON.parse(auth);
+}
+
+export function setAuth(authData: Auth | null, isRemember?: boolean) {
+  clearAuth();
+
+  if (authData == null)
+    return;
+
+  const storage = isRemember == true ? localStorage : sessionStorage;
+
+  if (storage == null)
+    return;
+
+  storage['mirror.auth'] = JSON.stringify(authData);
+
+}
+
+export function clearAuth() {
+  if (sessionStorage != null) {
+    sessionStorage.removeItem('mirror.auth');
+  }
+  if (localStorage != null) {
+    localStorage.removeItem('mirror.auth');
+  }
+}
+
+export interface Auth {
+  id: string;
+  login: string;
+  name: string;
+}
