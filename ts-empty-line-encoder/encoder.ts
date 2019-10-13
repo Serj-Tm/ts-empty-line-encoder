8d8f8e9c3d70f6657ad98289ef4134ff0b4f70ca
- <->
2019-10-13 21:57:15 +0300

    newLine options added

--- ts-empty-line-encoder/encoder.ts
+++ ts-empty-line-encoder/encoder.ts
@@ -1,36 +1,39 @@
 export class EmptyLineEncoder{
     static defaultEmptyLineMarker:string = "!--empty-line--!";
-    constructor(emptyLineMarker?: string){
+    static defaultNewLine:string = '\r\n';
+    constructor(emptyLineMarker?: string, newLine?: string){
         this.emptyLineMarker = emptyLineMarker || EmptyLineEncoder.defaultEmptyLineMarker;
+        this.newLine = newLine || EmptyLineEncoder.defaultNewLine;
     }
     emptyLineMarker: string;
+    newLine: string;
 
     encode(text:string){
-        return encodeEmptyLines(text, this.emptyLineMarker);
+        return encodeEmptyLines(text, this.emptyLineMarker, this.newLine);
     }
     decode(text:string){
-        return decodeEmptyLines(text, this.emptyLineMarker);
+        return decodeEmptyLines(text, this.emptyLineMarker, this.newLine);
     }
 }
 
-export function encodeEmptyLines(text:string, emptyLineMarker?:string){
+export function encodeEmptyLines(text:string, emptyLineMarker?:string, newLine?:string){
     const marker = toComment(emptyLineMarker || EmptyLineEncoder.defaultEmptyLineMarker);
 
     const lines = text.split(/\r?\n/);
     
     const commentedLines = lines.map(line => line.trim() == '' ? marker : line);
     
-    return commentedLines.join('\r\n');
+    return commentedLines.join(newLine || '\r\n');
 }
 
-export function decodeEmptyLines(text:string, emptyLineMarker?:string){
+export function decodeEmptyLines(text:string, emptyLineMarker?:string, newLine?:string){
     const marker = toComment(emptyLineMarker || EmptyLineEncoder.defaultEmptyLineMarker);
 
     var lines = text.split(/\r?\n/);
 
     const uncommentedLines = lines.map(line => line == marker ? '' : line);
     
-    return uncommentedLines.join('\r\n');
+    return uncommentedLines.join(newLine || '\r\n');
 }
 
 function toComment(marker:string){
